3a64ac8d0ba432f31a153a989350c3c19a674b42
FriedemannClaus <36330025+FriedemannClaus@users.noreply.github.com>
2024-07-30 15:29:16 +0200

    Implemented Shader Class

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 
 #include "shared/data.h"
 #include "shared/functions.h"
+#include "Shader.hpp"
 
 double lastTime = 0.0;
 int frameCount = 0;
@@ -21,6 +22,7 @@ float fps = 0.0f;
 void updateFPS() {
 	double currentTime = glfwGetTime();
 	frameCount++;
+#include <Shader.hpp>
 
 	if (currentTime - lastTime >= 1.0) { // Every second
 		fps = frameCount / static_cast<float>(currentTime - lastTime);
@@ -42,35 +44,33 @@ int main(int argc, char** argv)
 	GLFWwindow* window = initAndCreateWindow();
 	glViewport(0, 0, WIDTH, HEIGHT);
 
-	// std::cout << std::filesystem::path(ROOT_DIR) / "res/shader.frag" << std::endl;
-	std::filesystem::path simpleVertexShaderPath = std::filesystem::path(ROOT_DIR) / "res/shader.vert";
-	std::filesystem::path simpleFragmentShaderPath = std::filesystem::path(ROOT_DIR) / "res/shader.frag";
+    std::filesystem::path simpleVertexShaderPath = std::filesystem::path(ROOT_DIR) / "res/shader.vert";
+    std::filesystem::path simpleFragmentShaderPath = std::filesystem::path(ROOT_DIR) / "res/shader.frag";
 
-	GLint shaderProgram = createShaderPipelineFromPaths(simpleVertexShaderPath, simpleFragmentShaderPath);
+    Shader shader(simpleVertexShaderPath, simpleFragmentShaderPath);
 
-	GLuint vao, vbo;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
-	/* Position attribute */
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	/* Color attribute */
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-	glBindVertexArray(0);
-	glUseProgram(shaderProgram);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_TRIANGLES);
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
 
-	while (glfwWindowShouldClose(window) == 0)
-	{
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
 
-		// clear the window
-		glClearColor(0.0f, 0.1f, 0.2f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(0);
+
+    shader.use();
+    glPolygonMode(GL_FRONT_AND_BACK, GL_TRIANGLES);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.0f, 0.1f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -87,5 +87,9 @@ int main(int argc, char** argv)
 		glfwPollEvents();
 	}
 
-	glfwTerminate();
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glfwTerminate();
+
+    return 0;
 }
