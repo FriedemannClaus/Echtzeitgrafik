cfa1dc5badfbd0bc74b32fbcb20a455cc7a16989
acma1012 <acma1012@h-ka.de>
2024-07-31 14:04:16 +0200

    adjusted size calculation in geometry buffer

--- src/GeometryBuffer.cpp
+++ src/GeometryBuffer.cpp
@@ -44,7 +44,9 @@ void GeometryBuffer::initialize(const float* vertices, int size) {
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), vertices, GL_STATIC_DRAW);
+    //sizeof(cube) already gives the correct byte size on linux. no need to multiply by float size
+    //glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
